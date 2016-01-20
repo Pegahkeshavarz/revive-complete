eadb73b72154680940c71578a3c5b7f56c6d4c89
Pegahkeshavarz <pegah@reviveapp.com>
2016-01-20 01:54:43 -0800

    added

---
+++ js/landing-page.js
@@ -0,0 +1,60 @@
+//Animation for text and image
+
+(function( $ ) {
+
+    //Function to animate slider captions 
+    function doAnimations( elems ) {
+        //Cache the animationend event in a variable
+        var animEndEv = 'webkitAnimationEnd animationend';
+        
+        elems.each(function () {
+            var $this = $(this),
+                $animationType = $this.data('animation');
+            $this.addClass($animationType).one(animEndEv, function () {
+                $this.removeClass($animationType);
+            });
+        });
+    }
+    
+    //Variables on page load 
+    var $myCarousel = $('#carousel-hero'),
+        $firstAnimatingElems = $myCarousel.find('.item:first').find("[data-animation ^= 'animated']");
+        
+    //Initialize carousel 
+    $myCarousel.carousel();
+    
+    //Animate captions in first slide on page load 
+    doAnimations($firstAnimatingElems);
+    
+    //Pause carousel  
+    //$myCarousel.carousel('pause');
+    
+    
+    //Other slides to be animated on carousel slide event 
+    $myCarousel.on('slide.bs.carousel', function (e) {
+        var $animatingElems = $(e.relatedTarget).find("[data-animation ^= 'animated']");
+        doAnimations($animatingElems);
+    });
+    
+    //WOW Animation
+    //new WOW().init();
+    
+    //Tooltip
+    $('[data-toggle="tooltip"]').tooltip();
+    
+    //Page Scroll
+    $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top -84}, 1000);
+        return false;
+      }
+    }
+  });
+    
+    
+})(jQuery);
+
